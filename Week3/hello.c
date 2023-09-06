c96ba1d4049602ad0d473d2f7ebdc23a87b7c642
EkaterinaMiller <evmiller@nic.edu>
2023-09-06 15:46:32 -0700

    Week 3

---
+++ Week3/hello.c
@@ -0,0 +1,15 @@
+/**
+ * @file hello.c
+ * 
+ * Streams Hello, world! to stdout
+ * 
+ * @author Gabe de la Cruz
+ * @date   September 3, 2020
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
