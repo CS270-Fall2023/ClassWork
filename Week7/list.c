ccda45f0d86bf4ee33bb3a8a7608c71b7f66a9b9
EkaterinaMiller <43764013+EkaterinaMiller@users.noreply.github.com>
2023-10-04 21:16:51 +0000

    List is done

--- Week7/list.c
+++ Week7/list.c
@@ -6,11 +6,18 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/sysmacros.h>
+#include <pwd.h>
+#include <grp.h> 
+#include <time.h>
 
 #define MAXPATH 512
 
 void list(const char *dirName);
 void showStatInfo(const char *fname, struct stat *buf);
+void modeToLetters(int mode, char str[]);
+char * uidToName(uid_t id);
+char * gidToName(uid_t id);
 
 int main(int argc, char * argv[]){
     if (argc ==1){
@@ -56,12 +63,59 @@ void showStatInfo(const char *fname, struct stat *buf)
 {
     if (fname[0] == '.')//don't display infor for hiden files
         return;
+    char strMode[11];
+    
+    //printf("   mode: %o\n", buf->st_mode);
+    modeToLetters(buf->st_mode, strMode);
+    //printf("   mode: %s\n", strMode);
+    printf("%-12s", strMode);
+    //printf("  links: %ld\n", buf->st_nlink);
+    printf("%-3ld", buf->st_nlink);
+    //printf("   user: %d\n", buf->st_uid);
+    //printf("   user: %s\n", uidToName(buf->st_uid));
+    printf("%-12s", uidToName(buf->st_uid));
+    
+    //printf("  group: %d\n", buf->st_gid);
+    //printf("  group: %s\n", gidToName(buf->st_gid));
+    printf("%-12s", gidToName(buf->st_gid));
+    //printf("   size: %ld\n", buf->st_size);
+    printf("%6ld", buf->st_size);
 
-    printf("   name: %s\n", fname);
-    printf("   mode: %o\n", buf->st_mode);
-    printf("  links: %ld\n", buf->st_nlink);
-    printf("   user: %d\n", buf->st_uid);
-    printf("  group: %d\n", buf->st_gid);
-    printf("   size: %ld\n", buf->st_size);
-    printf("modtime: %ld\n\n", buf->st_mtime);
+    //printf("modtime: %ld\n\n", buf->st_mtime);
+    //printf("modtime: %.12s\n\n", 4 + ctime(&(buf->st_mtime)));
+    printf("%14.12s", 4 + ctime(&(buf->st_mtime)));
+    //printf("   name: %s\n", fname);
+    printf("  %s\n", fname);
 }
+
+void modeToLetters(int mode, char str[])
+{
+    strcpy(str, "----------");
+    // REST OF CODE HERE
+    if (S_ISDIR(mode)) str[0]='d';
+    else if (S_ISCHR(mode)) str[0]='c';
+    else if (S_ISBLK(mode)) str[0] = 'b';
+
+    if (mode & S_IRUSR)  str[1]='r';
+    if (mode & S_IWUSR)  str[2]='w';
+    if (mode & S_IXUSR)  str[3]='x';
+
+    if (mode & S_IRGRP)  str[4]='r';
+    if (mode & S_IWGRP)  str[5]='w';
+    if (mode & S_IXGRP)  str[6]='x';
+
+    if (mode & S_IROTH)  str[7]='r';
+    if (mode & S_IWOTH)  str[8]='w';
+    if (mode & S_IXOTH)  str[9]='x';
+}
+
+char * uidToName(uid_t id){
+    return getpwuid(id)->pw_name;
+
+}
+
+char * gidToName(uid_t id){
+    return getgrgid(id)->gr_name;
+
+}
+
