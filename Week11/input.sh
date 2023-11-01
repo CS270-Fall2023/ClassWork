8874217d773c1350716b7be732354fc12ac765de
EkaterinaMiller <evmiller@nic.edu>
2023-11-01 14:23:48 -0700

    Week11 is done

---
+++ Week11/input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Example of reading a variable
+echo -n "Please enter your name: "
+read firstName lastName             # read just two variables
+echo your first name is $firstName
+echo your last name is $lastName
+            
