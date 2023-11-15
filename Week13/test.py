ef0eaf445964c4d0c4f671fc1ec434775e801d4b
EkaterinaMiller <evmiller@nic.edu>
2023-11-15 15:55:17 -0800

    Week13 is done

--- Week13/test.py
+++ Week13/test.py
@@ -2,13 +2,19 @@
 import myFunctions
 #from myFunctions import repeat
 import os, platform
+import sys
 
 
 
 def main():
     #print('Your System: ' + platform.system())
-    c = os.getcwd()
-    myFunctions.listDir(c)
+    print(sys.argv)
+    for file in sys.argv[1:]:
+        print(file)
+
+
+    # c = os.getcwd()
+    # myFunctions.listDir(c)
 
     # print(myFunctions.repeat('Yay ', False))      ## YayYayYay
     # print(myFunctions.repeat('Woo Hoo ', True))   ## Woo HooWoo HooWoo Hoo!!!
